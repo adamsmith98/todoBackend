ef62b6f553fbfca1050fb940aca7f5af3869821a
Adam Smith <adamcsmith98@gmail.com>
2016-11-02 22:50:49 +0000

    Delete functionality

--- server.js
+++ server.js
@@ -16,29 +16,35 @@ mongoose.connect('mongodb://localhost:27017/todos');
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
   next();
 });
 
-router.get('/', (req, res) => {
-  Todo.find((err, todos) => {
-    if (err) res.send(err);
-    res.send(todos);
+router.route('/')
+  .get((req, res) => {
+    Todo.find((err, todos) => {
+      if (err) res.send(err);
+      res.json(todos);
+    });
   })
-});
+  .post((req, res) => {
+    let todo = new Todo();
+    todo.title = req.body.title;
+    todo.deadline = req.body.deadline;
+    todo.priority = req.body.priority;
+    todo.save((err) => {
+      if (err) res.send(err);
+      res.json(todo);
+    });
+  });
 
-router.post('/', (req, res) => {
-  let todo = new Todo();
-  todo.title = req.body.title;
-  todo.deadline = req.body.deadline;
-  todo.priority = req.body.priority;
-  todo.save((err) => {
+router.delete('/:id', (req, res) => {
+  Todo.remove({
+    _id: req.params.id
+  }, (err, todo) => {
     if (err) res.send(err);
     res.json(todo);
   });
-});
-
-router.delete('/', (req, res) => {
-  Todo.remove({});
 });
 
 app.use('/', router);
